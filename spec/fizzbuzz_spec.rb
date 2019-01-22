576cbd6973a1be79a9619dedacf1466a95f5b49d
Simon Norman <01secn@gmail.com>
2019-01-22 11:29:05 +0000

    Test that fizzbuzz returns parameter if not multiple of 3 or 5 enhanced, new conditions added

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -6,6 +6,9 @@ describe 'fizzbuzz' do
   
   it 'returns parameter if parameter is not a multiple of 3 or 5' do
     expect(fizzbuzz(4)).to eq 4
+    expect(fizzbuzz(7)).to eq 7
+    expect(fizzbuzz(11)).to eq 11
+    expect(fizzbuzz(98)).to eq 98
   end
 end
 

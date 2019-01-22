d86723903937ef292c593c3331d4aec71c1de057
Sarah Byrne <sarah.byrne@johnlewis.co.uk>
2019-01-22 11:44:08 +0000

    add code to pass fizz if multiple of 3 and buzz if multiple of 5

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,7 +1,9 @@
 def fizzbuzz(number)
   if number % 3 != 0 && number % 5 != 0
     return number
-  else
-    'fizz'
+  elsif number % 3 == 0
+    return 'fizz'
+  elsif number % 5 == 0
+    return 'buzz'
   end
 end
